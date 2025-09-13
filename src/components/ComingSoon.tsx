ce596481c85ca7b11cf7587e8fe587a174527ed6
Tycoon <tycoon@Tycoons-MacBook-Pro.local>
2025-09-13 21:12:10 +0200

    timmer working

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
 import video from "../assets/1851190-uhd_3840_2160_25fps.mp4";
 
 import logo from "../assets/Logo PNG_Plan de travail 1 copie.png";
 const ComingSoon = () => {
+  const [timeLeft, setTimeLeft] = useState({
+    days: "",
+    hours: "",
+    minutes: "",
+    seconds: "",
+  });
+
+  const countDown = () => {
+    const targetDate = new Date("2025-09-22").getTime();
+    const todayDate = Date.now();
+    const difference = targetDate - todayDate;
+    setTimeLeft({
+      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+      minutes: Math.floor((difference / (1000 * 60)) % 60),
+      seconds: Math.floor((difference / 1000) % 60),
+    });
+  };
+
+  useEffect(() => {
+    countDown();
+    const timer = setInterval(countDown, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <div className="main-coming-soon">
       <video autoPlay loop muted playsInline className="video-bg">
@@ -18,21 +44,21 @@ const ComingSoon = () => {
 
         <div className="countdown">
           <div className="countdown-item">
-            <p className="countdown-number">30</p>
+            <p className="countdown-number">{timeLeft.days}</p>
             <p className="countdown-label">Days</p>
           </div>
 
           <div className="countdown-item">
-            <p className="countdown-number">10</p>
+            <p className="countdown-number">{timeLeft.hours}</p>
             <p className="countdown-label">Hours</p>
           </div>
 
           <div className="countdown-item">
-            <p className="countdown-number">20</p>
+            <p className="countdown-number">{timeLeft.minutes}</p>
             <p className="countdown-label">Minutes</p>
           </div>
           <div className="countdown-item">
-            <p className="countdown-number">10</p>
+            <p className="countdown-number">{timeLeft.seconds}</p>
             <p className="countdown-label">Seconds</p>
           </div>
         </div>
